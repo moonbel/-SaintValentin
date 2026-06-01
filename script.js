dd8998465ae3aa1cff4684d204b15900b641abac
moonbel <hizabelesouza@gmail.com>
2026-06-01 19:40:49 -0300

    namoridos

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let global = "Eu sou uma variável global"
+function funcao1(){
+let local = "Eu sou uma variável local"
+console.log(local)
+console.log(global)
+}
+function funcao2(){
+console.log(global)
+// console.log(local)
+}
+function frasesFofinhas(){
+    cont =+ frasesFofinhas <p>"A vida ao seu ladoé a melhor coisa que existe, por que contigo eu me sinto leve e os problemas da vida somem, e fica apenas você"
+    cont === frasesFofinhas
+    document.getElementById.innerHTML = cont
+    
+}                         
